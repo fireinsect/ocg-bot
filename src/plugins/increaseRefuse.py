aa47bc900d4ca3e70bc4941f383357f5fd531281
fireinsect <847954981@qq.com>
2023-06-09 22:00:20 +0800

    perf: 解决了风控导致强制拉群检测失效的问题

--- src/plugins/increaseRefuse.py
+++ src/plugins/increaseRefuse.py
@@ -1,3 +1,5 @@
+import logging
+
 from nonebot.typing import T_State
 from nonebot.adapters.cqhttp import Event, Bot
 from nonebot.adapters.cqhttp.event import GroupRequestEvent, GroupIncreaseNoticeEvent
@@ -15,7 +17,8 @@ async def _(bot: Bot, event: GroupRequestEvent):
     addGroupId = event.group_id
     addGroupRequestTime = event.time
 
-time_ex=4
+
+time_ex = 4
 
 jiaqunNo = on_notice()
 
@@ -24,6 +27,10 @@ jiaqunNo = on_notice()
 async def _(bot: Bot, event: GroupIncreaseNoticeEvent):
     if event.group_id == addGroupId:
         if event.time == addGroupRequestTime or event.time <= addGroupRequestTime + time_ex:
-            await jiaqunNo.send("检测到非同意拉群")
-            await jiaqunNo.send("已退出群聊")
-            await bot.set_group_leave(group_id=event.group_id,is_dismiss=False,self_id=bot.self_id)
+            try:
+                await jiaqunNo.send("检测到非同意拉群")
+                await jiaqunNo.send("已退出群聊")
+            except Exception as e:
+                logging.INFO("group_" + event.group_id + "强制拉群")
+            finally:
+                await bot.set_group_leave(group_id=event.group_id, is_dismiss=False, self_id=bot.self_id)
